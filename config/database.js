f4bc4e377ccb0390fcb6ba557e9511f4f01b9e34
dessHub <deskip95@gmail.com>
2017-03-02 10:37:15 +0300

    v

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports=[
 
 		{
 	    'environment': 'production',
-	    'url'       : 'mongodb://localhost/aspirantdb',
+	    'url'       : 'mongodb://desshub:1ncorrect.@ds157459.mlab.com:57459/aspirantdb',
 	    'secret'    : 'anystringoftext'
 	  },
 
